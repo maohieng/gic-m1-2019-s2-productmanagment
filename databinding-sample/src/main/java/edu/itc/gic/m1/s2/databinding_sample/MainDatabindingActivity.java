f412e92eced8e5b3e2411167caef511444eaa784
Hieng <maohieng@gmail.com>
2020-03-18 08:45:55 +0700

    module databinding-sample

---
+++ databinding-sample/src/main/java/edu/itc/gic/m1/s2/databinding_sample/MainDatabindingActivity.java
@@ -0,0 +1,14 @@
+package edu.itc.gic.m1.s2.databinding_sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainDatabindingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_databinding);
+    }
+}
