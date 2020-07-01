b12e961a54d29e659269de0cfe24a0d65aa8e051
maohieng <maohieng@gmail.com>
2020-07-01 10:30:40 +0700

    user location

--- user-location-sample/src/main/java/com/example/user_location_sample/MainActivity.java
+++ user-location-sample/src/main/java/com/example/user_location_sample/MainActivity.java
@@ -1,26 +1,52 @@
 package com.example.user_location_sample;
 
 import android.Manifest;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity implements OnSuccessListener<Location> {
 
     private static final String TAG = "MainActivity";
 
+    public static final String EXTRA_REQUEST_LOCATION_UPDATE = "EXTRA_REQUEST_LOCATION_UPDATE";
+
     public static final int LOCATION_REQUEST_CODE = 121;
+    public static final int REQUEST_CHECK_SETTINGS = 111;
 
     FusedLocationProviderClient fusedLocationProviderClient;
+    LocationRequest locationRequest;
+    LocationCallback locationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            if (locationResult != null) {
+                Location lastLocation = locationResult.getLastLocation();
+                Log.i(TAG, "onLocationResult: lastLocation = " + lastLocation);
+            }
+        }
+    };
+
+    boolean requestLocationUpdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +65,22 @@ public class MainActivity extends AppCompatActivity implements OnSuccessListener
         // get last know location of user
         fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this);
 
+        // Create location request to request update
+        createLocationRequest();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!requestLocationUpdate) {
+            startLocationUpdate();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdate();
     }
 
     @Override
@@ -53,9 +95,58 @@ public class MainActivity extends AppCompatActivity implements OnSuccessListener
 
     @Override
     public void onSuccess(Location location) {
+        // Logic to check location accuracy by our selve
         Log.i(TAG, "onSuccess: Current user location = " + location);
         if (location != null) {
-            // TODO: 7/1/20 do something with location
+            Log.i(TAG, "onSuccess: latitude = " + location.getLatitude());
+            Log.i(TAG, "onSuccess: longitude = " + location.getLongitude());
         }
+    }
+
+    private void startLocationUpdate() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+        requestLocationUpdate = true;
+    }
+
+    private void stopLocationUpdate() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+        requestLocationUpdate = false;
+    }
+
+    private void createLocationRequest() {
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(10000L);
+        locationRequest.setFastestInterval(5000L);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        // Get current location settings
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+        SettingsClient settingsClient = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = settingsClient.checkLocationSettings(builder.build());
+        task.addOnSuccessListener(new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                boolean gpsPresent = locationSettingsResponse.getLocationSettingsStates().isGpsPresent();
+                Log.i(TAG, "checkLocationSettings->onSuccess: gpsPresent = " + gpsPresent);
+            }
+        });
+        task.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof ResolvableApiException) {
+                    // location settings is not satified.
+                    ResolvableApiException resolvable = (ResolvableApiException) e;
+                    try {
+                        resolvable.startResolutionForResult(MainActivity.this, REQUEST_CHECK_SETTINGS);
+                    } catch (IntentSender.SendIntentException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        });
     }
 }
